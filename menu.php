8b181b82a2d911b985e97838445d0688e5a610fb
monirul89 <monirul89@gmail.com>
2017-08-21 22:52:57 +0600

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="css/styles.css">
+        <link type="text/css" rel="stylesheet" href="css/menu.css">
+        <script src="js/customJavaScript.js"></script>
+        <title>Menu</title>
+    </head>
+    <body>
+        <header>
+            <div id="header_area">
+                <div class="container">
+                    <div id="menu_area">
+                        <ul>
+                            <li>
+                                <a href="#">Stacking Plans</a>
+                                <ul>
+                                    <li><a href="#">Item-1</a></li>
+                                    <li><a href="#">Item-2</a>
+                                        <ul>
+                                            <li><a href="#">Item-2.1</a></li>
+                                            <li><a href="#">Item-2.2</a></li>
+                                            <li><a href="#">Item-2.3</a></li>
+                                        </ul>								
+                                    </li>
+                                    <li><a href="#">Item-3</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">Assumptions</a>
+                            </li>
+                            <li>
+                                <a href="#">Views</a>
+                            </li>
+                            <li>
+                                <a href="#">Tools</a>
+                                <ul>
+                                    <li><a href="#">Item-1</a></li>
+                                    <li><a href="#">Item-2</a>
+                                        <ul>
+                                            <li><a href="#">Item-2.1</a></li>
+                                            <li><a href="#">Item-2.2</a></li>
+                                            <li><a href="#">Item-2.3</a></li>
+                                        </ul>								
+                                    </li>
+                                    <li><a href="#">Item-3</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">User System</a>
+                            </li>
+                        </ul>		
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section>
+            <div class="container">
+
+            </div>
+        </section>
+    </body>
+</html>
+
+
